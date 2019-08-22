a6f41f0df8d461495b25727f44be4b6f2191ad59
kalle <kalle.kettunen@outlook.com>
2019-08-22 23:10:07 +0300

    phase 4 fixed error

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        ApplicationDbContext(DbContextOptions options) : base(options)
+        ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
