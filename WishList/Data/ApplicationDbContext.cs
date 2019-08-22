1d4479a80c7f4f0131fca263ca0e4695137a95da
kalle <kalle.kettunen@outlook.com>
2019-08-22 23:07:55 +0300

    phase 4

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }  
+    }
+}
